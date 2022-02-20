63bb41e45e535a37a3779553235b7a171a9f1f86
Anup <anupleo5@gmail.com>
2022-02-20 21:56:48 +0545

    All components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path = "/">
+          <Home/>
+        </Route>
+        <Route path = "/login">
+          <Login/>
+        </Route>
+        <Route path = "/register">
+          <Register/>
+        </Route>
+        <Route path = "/products">
+          <ProductList/>
+        </Route>
+        <Route path = "/">
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
+
+  );
 };
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Categories from "../components/Categories";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
