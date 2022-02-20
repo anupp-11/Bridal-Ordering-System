e59473e385092a73828fb4fe02990794e7c43cf3
Anup <anupleo5@gmail.com>
2022-02-20 21:04:31 +0545

    All Components Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-// import Announcement from "../components/Announcement";
-// import Categories from "../components/Categories";
-// import Footer from "../components/Footer";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-// import Newsletter from "../components/Newsletter";
-// import Products from "../components/Products";
-// import Slider from "../components/Slider";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
-      {/* <Announcement /> */}
       <Navbar />
-      {/* <Slider />
+      <Slider />
       <Categories />
       <Products/>
-      <Newsletter/>
-      <Footer/> */}
+      <Footer/>
     </div>
   );
 };
